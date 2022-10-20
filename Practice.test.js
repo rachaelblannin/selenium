0bda50e0d21cecee4e3077709a7ea83fe4ff0fb3
rachaelblannin <rachblannin@gmail.com>
2022-10-20 16:14:42 +0100

    commit

--- Practice.test.js
+++ Practice.test.js
@@ -18,3 +18,12 @@ it("should find a dress", async () => {
     }
 }); 
 })
+
+await driver.findElement(By.css("#main-content > form > div > div > div > button")).click();
+      const result = await driver.findElement(By.css("#main-content > div.ssrcss-1v7bxtk-StyledContainer.enjd40x0 > div > div > ul > li:nth-child(1) > div > div > div.ssrcss-tq7xfh-PromoContent.e1f5wbog7 > div.ssrcss-1f3bvyz-Stack.e1y4nx260 > a > span > p > span"));
+      expect(await result.getText()).to.equal("The Man Who Made Mock Turtle Soup");
+    } finally {
+      await driver.quit();
+    }
+  })
+  
