cf565615144f7b6c44b17a1eabea20818de8bb78
rachaelblannin <rachblannin@gmail.com>
2022-10-21 11:22:52 +0100

    commit

--- Practice.test.js
+++ Practice.test.js
@@ -7,7 +7,7 @@ describe("selenium tests", function () {
   this.timeout(30_000);
 
 
-it("should find a dress", async () => {
+/* it("should find a dress", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
       await driver.get("https://www.automationpractice.com/index.php");
@@ -17,13 +17,17 @@ it("should find a dress", async () => {
        await driver.quit(); 
     }
 }); 
-})
+}) */
 
-await driver.findElement(By.css("#main-content > form > div > div > div > button")).click();
-      const result = await driver.findElement(By.css("#main-content > div.ssrcss-1v7bxtk-StyledContainer.enjd40x0 > div > div > ul > li:nth-child(1) > div > div > div.ssrcss-tq7xfh-PromoContent.e1f5wbog7 > div.ssrcss-1f3bvyz-Stack.e1y4nx260 > a > span > p > span"));
-      expect(await result.getText()).to.equal("The Man Who Made Mock Turtle Soup");
+it("3.2", async () => {
+  const driver = await new Builder().forBrowser("chrome").build();
+  try {
+    await driver.get("http://localhost:3000/testing/ex1");
+    const search= await driver.findElement(By.css("#root > div > div.container > div:nth-child(2) > div:nth-child(2) > input"));
+    await search.sendKeys("2", Key.ENTER);
+      const result = await driver.findElement(By.css("#root > div > div.container > div:nth-child(2) > div.mt-4.input-group > input"));
+      expect(await result.getAttribute("value")).to.equal("3.2");
     } finally {
       await driver.quit();
     }
-  })
-  
+})})
